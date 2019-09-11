4e9e3a57488dfb0a149c35dbc94d590e05ee9554
Isabellawwq <arisaploy_557@hotmail.com>
2019-09-11 23:46:40 +0700

    ASS6_SEC1_603020784-4

---
+++ app/src/main/java/com/example/ass6/OneFragment.kt
@@ -0,0 +1,24 @@
+package com.example.ass6
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class OneFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_one, container, false)
+        return view
+    }
+
+
+}
